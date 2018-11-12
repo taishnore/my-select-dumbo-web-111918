99ee13fb993735605ca0d7076a0640c85e559750
Taimur Shah <tshah@scu.edu>
2018-11-12 22:07:53 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,11 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  i = 0
+  test = []
+  while i < array.length
+    if yield(array[i]) == true
+      test.push(array[i])
+    end
+    i += 1
+  end
+  test
 end
